2e178b7bf3bd000574da4e3fd6ea376df847c4eb
RtoCast <angelretocastro@gmail.com>
2025-03-28 23:53:09 -0500

    Agregación del token de google analytics en link-bio.py + Prueba de api

--- link_bio/link_bio.py
+++ link_bio/link_bio.py
@@ -4,25 +4,28 @@ import link_bio.styles.styles as styles
 from link_bio.pages.index import index
 from link_bio.pages.courses import courses
 from link_bio.pages.certifications import certifications
+from link_bio.api.api import hello
 
 class State(rx.State):
     pass
     
 app = rx.App(
     stylesheets = styles.STYLESHEETS,
-    style = styles.BASE_STYLE
-    # head_components= [
-    #     rx.script(src=f"https://www.googletagmanager.com/gtag/js?id=G-QVRYTWM0N7"),
-    #     rx.script(
-    #         """        
-    #         "window.dataLayer = window.dataLayer || [];
-    #         function gtag(){dataLayer.push(arguments);}
-    #         gtag('js', new Date());
-    #         gtag('config', 'G-QVRYTWM0N7');
-    #         """
-    #     )            
-    # ]
+    style = styles.BASE_STYLE,
+    head_components= [
+        rx.script(
+            src=f"https://www.googletagmanager.com/gtag/js?id={constants.G_tAG}"
+        ),
+        rx.script(
+            f"""        
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){{dataLayer.push(arguments);}}
+            gtag('js', new Date());
+            gtag('config', '{constants.G_tAG}');
+            """
+        )            
+    ]
 )
 
-
+app.api.add_api_route("/hello",hello)
 
