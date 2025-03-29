577ed7405f485d71c0a86793bd7bdb95d180cb01
RtoCast <angelretocastro@gmail.com>
2025-03-28 21:20:26 -0500

    Efectuando cambio para tener listo el archivo build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
-.\.venv\Scripts\activate
-pip install --upgrade pip
+#Desde Windows
+#.\.venv\Scripts\activate
+#Desde Linux Mac o Gitbash
+#source .venv/bin/activate
+source .venv/Scripts/activate
+
+#pip install --upgrade pip
+".\.venv\Scripts\python.exe" -m pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-reflex export --frontend-only
+API_URL=https://rtocast-web.up.railway.app reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
+
+#Para Windows
+#.\.venv\Scripts\deactivate
+#Para Linux, Mac o Git Bash
 deactivate
